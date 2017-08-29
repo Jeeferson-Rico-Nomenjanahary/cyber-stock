6a50db9367c71d7ed5b63e5ce55cfe9fd2638442
Jeeferson-Rico-Nomenjanahary <nomenjanaharyrico@gmail.com>
2017-08-29 23:37:35 +0300

    fos user override template ok

---
+++ src/UtilisateurBundle/Entity/Utilisateur.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace UtilisateurBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Utilisateur
+ *
+ * @ORM\Table(name="cyber_utilisateur")
+ * @ORM\Entity(repositoryClass="UtilisateurBundle\Repository\UtilisateurRepository")
+ */
+class Utilisateur extends BaseUser
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
